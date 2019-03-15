8bc5b6c150d49845023c243b7394767c52bc7477
yago.esh <yago.esh@gmail.com>
2019-03-15 16:15:13 +0100

    Table test

--- HomeAccounts/src/main/java/yagoamani/HomeAccounts/Interface.java
+++ HomeAccounts/src/main/java/yagoamani/HomeAccounts/Interface.java
@@ -1,34 +1,79 @@
 package yagoamani.HomeAccounts;
 
-import javax.swing.JFrame;
-import javax.swing.JToolBar;
 import java.awt.BorderLayout;
-import javax.swing.JButton;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class Interface {
 
-	protected  JFrame frame;
+	protected JFrame frame;
+	private JTable table;
 
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Interface window = new Interface();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
 	public Interface() {
 		initialize();
 	}
 
+	/**
+	 * Initialize the contents of the frame.
+	 */
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JToolBar toolBar = new JToolBar();
-		frame.getContentPane().add(toolBar, BorderLayout.NORTH);
-		
-		JButton btnNewButton = new JButton("New button");
-		toolBar.add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("New button");
-		toolBar.add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("New button");
-		toolBar.add(btnNewButton_2);
-	}
 
+		JMenuBar menuBar = new JMenuBar();
+		frame.getContentPane().add(menuBar, BorderLayout.NORTH);
+
+		JMenu mnView = new JMenu("Vista");
+		menuBar.add(mnView);
+
+		JMenuItem mnItmMonth = new JMenuItem("Contabilidad Mes");
+		mnView.add(mnItmMonth);
+
+		JMenuItem mnItmYear = new JMenuItem("Contabilidad Anual");
+		mnView.add(mnItmYear);
+
+		JMenuItem mnItmDebts = new JMenuItem("Deudas");
+		mnView.add(mnItmDebts);
+
+		JMenuItem mnItmGraphics = new JMenuItem("Graficos");
+		mnView.add(mnItmGraphics);
+
+		String[] columnNames = { "Fecha", "Categoria", "Importe", "%", "Comentario" };
+		String[][] data = { { "01/03/2019", "AJUSTES", "-92,22", "100", "AJUSTE DE DEUDAS PENDIENTES" }, { "01/03/2019", "OCIO", "-10", "0", "SPOTIFY PUREZA" } };
+		table = new JTable(data, columnNames);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		table.getColumnModel().getColumn(0).setPreferredWidth(70);
+		JScrollPane scrollPane = new JScrollPane(table);
+
+		frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane.setColumnHeaderView(scrollPane_1);
+	}
 }
